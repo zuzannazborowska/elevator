4ec9a2420e1b177512ef9f39cf65085f68480e14
Zuzanna Zborowska <zuzanna.zborowska@vm.pl>
2025-05-11 19:08:04 +0200

    initial commit

---
+++ src/main/java/el/ev/ElevatorApplication.java
@@ -0,0 +1,13 @@
+package el.ev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/el/ev/ElevatorApplicationTests.java
@@ -0,0 +1,13 @@
+package el.ev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
